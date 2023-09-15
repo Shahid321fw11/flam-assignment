95d5a9e34b197aa302d83d91857948fbef640e1a
Shahid321fw11 <sidansari20@gmail.com>
2023-09-16 04:38:20 +0530

    design: little Changes in description

--- src/BottomSheet.js
+++ src/BottomSheet.js
@@ -12,19 +12,23 @@ const BottomSheet = () => {
 
   const handleOpen = () => {
     setSheetHeight(300);
-    setDescription("Fully open");
+    setDescription(
+      "I'm thrilled about the opportunity to join Flam, as it perfectly complements my passion for crafting elegant and responsive web applications. My proficiency in React, coupled with a keen design sensibility, positions me to actively contribute to Flam's mission of delivering outstanding user experiences through innovative technology"
+    );
     setButtonStates({ open: true, halfOpen: false, close: false });
   };
 
   const handleHalfOpen = () => {
     setSheetHeight(200);
-    setDescription("Half-open");
+    setDescription(
+      "At Flam, we are reimagining digital experiences via AI stack. We believe that the future of physical and digital content is immersive. With our proprietary AI image recognition, gen AI mocap, and AI avatar tech, we are democratizing 3D XR content creation and camera-first consumption at scale."
+    );
     setButtonStates({ open: false, halfOpen: true, close: false });
   };
 
   const handleClose = () => {
     setSheetHeight(100);
-    setDescription("Close");
+    setDescription("Let's Start");
     setButtonStates({ open: false, halfOpen: false, close: true });
   };
 
